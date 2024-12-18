92a4051c3e042ccb4ec52f637bd23489ceac7abc
Gökdeniz Genç <fbgokdeniz@gmail.com>
2024-12-18 13:11:19 +0300

    fix

--- Core/Utilities/Business/BusinessRules.cs
+++ Core/Utilities/Business/BusinessRules.cs
@@ -11,16 +11,17 @@ namespace Core.Utilities.Business
     {
         public static IResult Run(params IResult[] logics)
         {
-            foreach (var logic in logics) 
+            foreach (var logic in logics)
             {
                 if (!logic.Success)
                 {
-                return logic;
-                
+                    return logic;
+
                 }
-            
+
             }
             return null;
-    }
+        }
 
+    }
 }
